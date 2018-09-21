28e3e096893d84820a1ca55cb1aa902353348f57
Bryon Wilkins <Bryon.Wilkins@blackbaud.com>
2018-09-21 17:03:26 -0400

    Add tests and log/read commands

--- python/pomodoro_logger.py
+++ python/pomodoro_logger.py
@@ -1,18 +1,31 @@
 from pygame import mixer
+import sys
 from time import sleep
 import pytz
 from datetime import datetime
 import os
 import json
 fileDir = os.path.dirname(os.path.realpath('__file__'))
-ocarina = os.path.join(fileDir, '../OOT_Secret.wav')
+ocarina = os.path.join(fileDir, '..\OOT_Secret.wav')
 times = {'w': 25, 'r': 5}
 mixer.init(frequency=32000)
 
+startText = """(w)ork block,
+(b)reak block,
+(l)ogging a task,
+(r)ead the log
+> """
+
 def read():
-    block = input('Is this a (w)ork or (r)est block?(w or r) ')
-    if block == 'r':
+    block = input(startText)
+    if block == 'b':
         start_timer(times[block])
+    elif block == 'l':
+        task = input('What did you do? ')
+        write_to_log(task)
+    elif block == 'r':
+        if(log_exists()):
+            print_log()
     else:
         if(log_exists()):
             tasks = existing_tasks()
@@ -20,10 +33,7 @@ def read():
                 print(str(i) + ': ' + tasks[i])
             print(str(len(tasks)) + ': New Task')
             num = int(input('Choose a task '))
-            if(num == len(tasks)):
-                task = input('What are you working on now? ')
-            else:
-                task = tasks[num]
+            task = input('What are you working on now? ') if num == len(tasks) else tasks[num]
         else:
             task = input('What are you working on? ')
         start_timer(times[block])
@@ -31,33 +41,65 @@ def read():
     read()
 
 def write_to_log(task_name):
-    index = len(existing_tasks()) if log_exists() else 0;
-    with open(file_name(), 'a+') as f:
+    index = len(read_log()) if log_exists() else 0
+    with open(file_name_with_date(datetime.utcnow()), 'a+') as f:
         f.write(str(index) + ' ' + task_name + '\n')
 
-def existing_tasks():
-    with open(file_name()) as task_list:
+def read_log():
+    with open(file_name_with_date(datetime.utcnow())) as task_list:
         raw_tasks = task_list.readlines()
-    return list(map((lambda x: x.split()[1].strip()), raw_tasks))
+    return raw_tasks
+
+def print_log():
+    tasks = list(set(map((lambda x: x.strip()), read_log())))
+    tasks.sort()
+    for task in tasks:
+        print(task)
+
+def existing_tasks():
+    raw_tasks = read_log()
+    tasks = []
+    for line in raw_tasks:
+        tasks.append(' '.join(line.split()[1::]))
+    return list(set(map((lambda x: x.strip()), tasks)))
 
 def file_name():
     return datetime.utcnow().strftime('%d-%m-%y')+'.log'
 
+def file_name_with_date(date):
+    return date.strftime('%d-%m-%y.log')
+
 def log_exists():
-    return os.path.isfile(file_name())
+    return os.path.isfile(file_name_with_date(datetime.utcnow()))
 
 def start_timer(minutes):
-    seconds = minutes * 1
+    seconds = minutes * 60
     for time_since in range(0, seconds):
         m, s = divmod((seconds - time_since), 60)
         print("%02d:%02d" % (m, s))
         sleep(1)
-        print(chr(27) + "[2J") # clear terminal
+        clear_terminal()
     play_sound()
+
+def clear_terminal():
+    print(chr(27) + "[2J") # clear terminal
+    if(sys.platform == 'win32'):
+        os.system('cls')
 
 def play_sound():
     mixer.music.load(ocarina)
     mixer.music.play()
+
+"""
+TESTS
+"""
+def test_file_name():
+    print(file_name_with_date(datetime(1901,2,3)) == '03-02-01.log')
+
+test_file_name()
+"""
+END TESTS
+"""
 
 while(True):
     read()
